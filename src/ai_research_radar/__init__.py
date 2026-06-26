5291bf562beca96733d76430d9790bcba7a837dd
Masoob Alam <masoob0085@gmail.com>
2026-06-26 15:08:14 +0530

    Add example TOML config and export package version

--- src/ai_research_radar/__init__.py
+++ src/ai_research_radar/__init__.py
@@ -3,10 +3,13 @@
 from ai_research_radar.brief import Brief, Finding, compile_brief
 from ai_research_radar.config import RadarConfig, load_config
 
+__version__ = "0.1.0"
+
 __all__ = [
     "Brief",
     "Finding",
     "RadarConfig",
+    "__version__",
     "compile_brief",
     "load_config",
 ]
